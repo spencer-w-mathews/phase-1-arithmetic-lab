b59702d63fe53ce7721ca9d5aeafc45e8a4212a7
Spencer Mathews <smathews0906@gmail.com>
2022-06-07 08:34:30 -0500

    commit tree 9c12d52f79bccf2b02bc73e66a73ff53fd87c28f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1=62;
+let num2=1;
+let multiply = num1 * num2;
+
+let random = Math.round(Math.floor(Math.random())+1);
+
+let num3=14;
+let num4=10;
+let mod= num3%num4;
+
+let max = Math.max(20,2,3,4,5);
